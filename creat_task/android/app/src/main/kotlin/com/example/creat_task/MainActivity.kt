9736b1eb3a33db4e6a841a1b74effce9c0b6fba9
HabibaSayed15 <habiba.sayed15.hs@gmail.com>
2023-11-06 23:40:55 +0200

    fff

---
+++ creat_task/android/app/src/main/kotlin/com/example/creat_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.creat_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ creat_task/lib/creat.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreatT extends StatefulWidget {
+  const CreatT({super.key});
+
+  @override
+  State<CreatT> createState() => _CreatTState();
+}
+
+class _CreatTState extends State<CreatT> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
